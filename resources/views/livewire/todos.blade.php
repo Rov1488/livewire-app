a2efb0351199ba2aac51c5ccdeb0c9a9527c6f70
R.Pulatov <r.pulatov1488@gmail.com>
2025-04-08 14:42:11 +0500

    proekti repositoriyaga yubrish

---
+++ resources/views/livewire/todos.blade.php
@@ -0,0 +1,15 @@
+<div class="w-5/12 mx-auto">
+
+    <form wire:submit="add" class="flex items-center justify-between">
+        <input type="text" wire:model.live..5ms="todo" class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 focus:ring-opacity-75" placeholder="Add a new todo">
+
+        <button type="submit" class="inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-normal">Add</button>
+    </form>
+    <p>Current todo: {{$todo}}</p>
+    <ul>
+        @foreach($todos as $todo)
+            <li>{{$todo}}</li>
+        @endforeach
+    </ul>
+
+</div>
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Livewire\Todos;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use Livewire\Volt\Volt;
+
+/*Route::get('/', function () {
+    return view('welcome');
+})->name('home');*/
+
+Route::get('/', function () {
+    return Auth::check()
+        ? redirect()->route('dashboard')
+        : redirect()->route('login');
+})->name('home');
+
+Route::get('/todos', Todos::class);
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::middleware(['auth'])->group(function () {
+    Route::redirect('settings', 'settings/profile');
+
+    Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
+    Volt::route('settings/password', 'settings.password')->name('settings.password');
+    Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
+});
+
+require __DIR__.'/auth.php';
